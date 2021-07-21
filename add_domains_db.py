de6235c515fa32d1c88c5bc6cd9e1bb4bf6c7b5c
XorOrNot <fabio.meneses@gmail.com>
2021-07-21 16:00:51 -0400

    first commit

---
+++ add_domains_db.py
@@ -0,0 +1,61 @@
+# add the csv file to sqlite
+# # https://mungingdata.com/sqlite/create-database-load-csv-python/
+
+from datetime import date, timedelta
+import os
+import re
+import csv
+
+# determine yesterday's date
+yesterday = date.today()-timedelta(days=1)
+yesterday = yesterday.strftime("%Y-%m-%d")
+
+# define the data transformations 
+
+def length(domain):
+    dot = domain.find(".")
+    site = domain[:dot-1]
+    return len(site)
+
+def tld(domain):
+    domlen = len(domain)
+    dot = domain.find(".")
+    tld = domain[dot+1:domlen]
+    return tld
+
+def hasnum(domain):
+    if re.search(r'\d', domain):
+        return 1
+    else: 
+        return 0
+
+def hasdash(domain):
+    if re.search(r'-', domain):
+        return 1
+    else: 
+        return 0
+
+# opening file1 in reading mode and file2 in writing mode
+date = date.today()-timedelta(days=1) 
+file = f'./data/{date}.'
+
+with open(file+'txt', 'r') as f1, open(file+'csv', 'w') as f2:
+    reader = f1.read().splitlines()
+    writer = csv.writer(f2, delimiter=',')
+    # add header
+    header = ['domain','dateAdd','length','tld','hasNum','hasDash','isAlive']
+    writer.writerow(i for i in header)
+
+    # loop through the lines in file1 and write functions in file2 
+    for line in reader: 
+        writer.writerow([line, date, length(line), tld(line), hasnum(line), hasdash(line)])
+
+    # no need to close the file - already closed. 
+
+#remove original file 
+os.system(f'rm -f ./data/{date}.txt')
+
+
+
+
+
---
+++ download_domains.py
@@ -0,0 +1,42 @@
+import base64
+from datetime import date, timedelta
+import wget
+import shutil
+import os
+
+'''https://whoisds.com//whois-database/newly-registered-domains/MjAyMS0wNy0xOS56aXA=/nrd'''
+
+DS_URL = 'https://whoisds.com//whois-database/newly-registered-domains/$date/nrd'
+
+# fetching yesterday's date in formar YYYY-MM-DD
+yesterday = date.today()-timedelta(days=1)
+yesterday = yesterday.strftime("%Y-%m-%d")+".zip"
+
+# encoding to b64
+yesterday = base64.b64encode(yesterday.encode()).decode()
+
+# replace URL with the encoded date
+DS_URL_TODAY = DS_URL.replace("$date", yesterday)
+
+#print(DS_URL_TODAY) // just for tsting
+
+date = date.today()-timedelta(days=1) 
+
+# download the file to /data folder
+print("# downloading file")
+wget.download(DS_URL_TODAY, f"./data/{date}.zip")
+
+# unpack file in /data folder
+print("")
+print("# unpacking file")
+shutil.unpack_archive(f"./data/{date}.zip", "./data")
+
+# adjust names and remove archive
+os.system(f"rm -f ./data/{date}.zip")
+os.system(f"mv ./data/domain-names.txt ./data/{date}.txt")
+
+
+
+
+
+
