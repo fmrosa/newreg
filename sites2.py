65b8fb1c7f86958af22b9f94fa0ed927a1a09a1e
Stephen Brown <sbrown@C02F30DMMD6Q.lan>
2021-08-20 12:40:36 -0400

    Added logs

--- sites2.py
+++ sites2.py
@@ -27,11 +27,13 @@ async def setoolspost(index, session, rbiurls):
         params = {'init': 'true'}
     else:
         params = {'append': 'true'}
-    print(rbiurls)
+    #print(rbiurls)
     setools = "https://setools.netskope.io/api/v1/file/rbiurls/"
     headers = {'Authorization': 'Token ydwXBxilYFD1yap97XxAUuggt8GGR3g3uq8WBqxHsnmV1Ad4uG'}
     resp = await session.post(setools, data=rbiurls, headers=headers, params=params)
-    print(await resp.json())
+    #print(await resp.json())
+    with open("./logs/log_" + filedate, 'a') as f:
+                    f.write(await resp.json())
 
 
 async def getbatch(index, batch):
